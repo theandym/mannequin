0a16e30dbd2f2f5967618d7cfae669b11a7da76f
Andy Macdonald <andy@theandym.com>
2013-01-16 17:28:56 -0500

    added five individual paragraphs of lipsum

--- test/lib/mannequin/text_test.rb
+++ test/lib/mannequin/text_test.rb
@@ -4,8 +4,12 @@ require Pathname(__FILE__).ascend{|d| h=d+'test_helper.rb'; break h if h.file?}
 describe Mannequin::Text.new do
   subject { Mannequin::Text.new }
   describe "creating text" do
-    it "must have one paragraph of lorem ipsum" do
-      subject.must_respond_to(:lipsum_par_1)
+    it "must have five paragraphs of lorem ipsum" do
+      subject.must_respond_to(:lipsum_paragraph_1)
+      subject.must_respond_to(:lipsum_paragraph_2)
+      subject.must_respond_to(:lipsum_paragraph_3)
+      subject.must_respond_to(:lipsum_paragraph_4)
+      subject.must_respond_to(:lipsum_paragraph_5)
     end
   end
 end
